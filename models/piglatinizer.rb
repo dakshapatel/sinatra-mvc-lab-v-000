ff5dfc91013704bf79184d9517557112f244405e
Daksha Patel <dakshapatel100@gmail.com>
2018-07-18 19:49:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,10 +17,7 @@ class PigLatinizer
     words.join(" ")
   end
 
-  # def vowel?(letter)
-  #   letter.downcase
-  #   letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
-  # end
+  
 
 
 end
